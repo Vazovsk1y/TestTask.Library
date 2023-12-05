07d6949c64ea72c948a7828bbf40af58f475d53e
Vazovsk1y <anolrey.b@gmail.com>
2023-12-05 18:56:15 +0300

    -added domain project and some base components.

---
+++ src/Core/TestTask.Domain/Entities/Book.cs
@@ -0,0 +1,14 @@
+﻿using TestTask.Domain.Common;
+
+namespace TestTask.Domain.Entities;
+
+public class Book : Entity<BookId>
+{
+	public Book() : base() { }
+}
+
+
+public record BookId(Guid Value) : IValueId<BookId>
+{
+	public static BookId Create() => new(Guid.NewGuid());
+}
