ddd7796cc87eeb81530123db1ec334fb2899b192
Vazovsk1y <anolrey.b@gmail.com>
2023-12-05 20:12:30 +0300

    -added extra domain entities and defined book entity base properties.

---
+++ src/Core/TestTask.Domain/Entities/Author.cs
@@ -0,0 +1,17 @@
+﻿using TestTask.Domain.Common;
+
+namespace TestTask.Domain.Entities;
+
+public class Author : Entity<AuthorId>
+{
+	public required string FullName { get; set; }
+
+	public ICollection<Book> Books { get; set; } = new List<Book>();
+
+	public Author() : base() { }
+}
+
+public record AuthorId(Guid Value) : IValueId<AuthorId>
+{
+	public static AuthorId Create() => new(Guid.NewGuid());
+}
--- src/Core/TestTask.Domain/Entities/Book.cs
+++ src/Core/TestTask.Domain/Entities/Book.cs
@@ -4,6 +4,18 @@ namespace TestTask.Domain.Entities;
 
 public class Book : Entity<BookId>
 {
+	public required string ISBN { get; set; }
+
+	public required string Title { get; set; }
+
+	public ICollection<BookGenre> Genres { get; set; } = new List<BookGenre>();
+
+	public string? Description { get; set; }
+
+	public required AuthorId AuthorId { get; set; }
+
+	public Author Author { get; set; }
+
 	public Book() : base() { }
 }
 
